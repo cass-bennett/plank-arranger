827e881f801b285016b04ca66e19d8a6d8b4a0f7
cass-bennett <cassandra.bennett413@gmail.com>
2019-07-27 14:48:08 -0400

    Piece lengths now stored in u32 instead of f64
    - program assumes input will be in meters
    - piece lengths are rounded to nearest 0.1mm

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 extern crate bit_vec;
 use bit_vec::BitVec;
-extern crate ordered_float;
-use ordered_float::NotNan;
 
 use priority_queue::PriorityQueue;
+use std::cmp::Reverse;
 use std::fs::File;
 use std::io;
 use std::io::Read;
@@ -15,24 +14,23 @@ fn main() {
 #[allow(dead_code)]
 pub struct PlankArrangement<'a> {
     planks: Vec<Plank<'a>>,
-    pieces: Vec<f64>,
-    max_len: f64,
+    pieces: Vec<u32>,
+    max_len: u32,
 }
 
 pub struct Plank<'a> {
-    all_pieces: &'a Vec<f64>,
+    all_pieces: &'a Vec<u32>,
     usable_pieces: Vec<usize>,
     num_pieces: usize,
-    rem_len: f64,
+    total_len: u32,
     state_bv: BitVec,
-    bit_queue: PriorityQueue<(BitVec,usize),NotNan<f64>>,
+    bit_queue: PriorityQueue<(BitVec,usize),Reverse<u32>>,
 }
 
 impl<'a> Plank<'a> {
     pub fn new(n_p: usize,
-               max_len: f64,
                prev_bits: &BitVec,
-               pcs: &'a Vec<f64>, ) -> Plank<'a> {
+               pcs: &'a Vec<u32>, ) -> Plank<'a> {
         let mut available_pieces = Vec::new();
         let mut i = 0;
         while i < pcs.len() {
@@ -46,23 +44,21 @@ impl<'a> Plank<'a> {
             all_pieces: pcs,
             usable_pieces: available_pieces,
             num_pieces: 0,
-            rem_len: 0.0,
+            total_len: 0,
             state_bv: prev_bits.clone(),
             bit_queue: PriorityQueue::new(),
         };
-        ret.set_num_pieces(n_p,max_len);
+        ret.set_num_pieces(n_p);
         return ret;
     }
-    fn set_num_pieces(&mut self,
-                      n_p: usize,
-                      max_len: f64) {
+    fn set_num_pieces(&mut self, n_p: usize) {
         self.num_pieces = n_p;
-        self.rem_len = max_len;
+        self.total_len = 0;
         self.bit_queue = PriorityQueue::new();
 
         for i in 0..self.num_pieces {
             let index = self.usable_pieces.get(i).unwrap();
-            self.rem_len -= *self.all_pieces.get(*index).unwrap();
+            self.total_len += *self.all_pieces.get(*index).unwrap();
             self.state_bv.set(*index,true);
         }
         for i in self.num_pieces..self.usable_pieces.len() {
@@ -79,14 +75,10 @@ impl<'a> Plank<'a> {
         let bit_index = self.usable_pieces.get(*index).unwrap();
         let dest_index = self.usable_pieces.get(*index+1).unwrap();
         let future_len =
-            self.rem_len
-            - (*self.all_pieces.get(*dest_index).unwrap()
+            self.total_len
+            + (*self.all_pieces.get(*dest_index).unwrap()
                - *self.all_pieces.get(*bit_index).unwrap());
-        let wrapped_len = NotNan::new(future_len).unwrap();
-        self.bit_queue.push((self.state_bv.clone(),*index),wrapped_len);
-    }
-    pub fn pieces_too_big(&self) -> bool {
-        self.rem_len < 0.0
+        self.bit_queue.push((self.state_bv.clone(),*index),Reverse(future_len));
     }
     pub fn has_next(&self) -> bool {
         !self.bit_queue.is_empty()
@@ -97,12 +89,12 @@ impl<'a> Plank<'a> {
     #[allow(dead_code)]
     fn nth_pieces_equal(&self, i_1:&usize, i_2:&usize) -> bool {
         match self.nth_pieces_diff(i_1,i_2) {
-            Some(a) => a.abs() < 0.000000001,
+            Some(a) => a == 0,
             None    => false
         }
     }
     #[allow(dead_code)]
-    fn nth_pieces_diff(&self, i_1:&usize, i_2:&usize) -> Option<f64> {
+    fn nth_pieces_diff(&self, i_1:&usize, i_2:&usize) -> Option<u32> {
         let val_1 = self.usable_pieces.get(*i_1)
             .and_then(|x| self.all_pieces.get(*x));
         let val_2 = self.usable_pieces.get(*i_2)
@@ -114,7 +106,9 @@ impl<'a> Plank<'a> {
     }
     fn next(&mut self) {
         let ((bv,index),wrapped_len) = self.bit_queue.pop().unwrap();
-        self.rem_len = wrapped_len.into_inner();
+        self.total_len = match wrapped_len {
+            Reverse(a) => a
+        };
         self.state_bv = bv;
 
         self.state_bv.set(*self.usable_pieces.get(index).unwrap(),false);
@@ -133,7 +127,7 @@ impl<'a> Plank<'a> {
 }
 
 #[allow(dead_code)]
-fn get_pieces() -> (f64,Vec<f64>) {
+fn get_pieces() -> (u32,Vec<u32>) {
     // get filename from the user
     print!("input file: ");
     io::stdout().flush().unwrap();
@@ -146,14 +140,15 @@ fn get_pieces() -> (f64,Vec<f64>) {
     let mut file = File::open(&file_name).expect("Couldn't open the file");
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("Couldn't read the file");
-    let mut v: Vec<f64> = contents.split_whitespace()
+    let mut v: Vec<u32> = contents.split_whitespace()
         .filter_map(|s| s.parse::<f64>().ok())
+        .map(|f| (f * 10000.0 + 0.5) as u32)
         .collect::<Vec<_>>();
     
     // take the first number in the file as the plank length
     let plank_len = v.remove(0);
     // sort the numbers from low to high
-    v.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    v.sort();
     v.shrink_to_fit();
 
     return (plank_len,v);
@@ -165,33 +160,33 @@ mod tests {
     
     #[test]
     fn test_plank_initializer() {
-        let pieces = vec![1.0, 2.0, 3.0, 4.0, 5.0];
-        let mut p = Plank::new(2,4.0,&BitVec::from_bytes(&[0x00]),&pieces);
+        let pieces = vec![1, 2, 3, 4, 5];
+        let mut p = Plank::new(2,&BitVec::from_bytes(&[0x00]),&pieces);
         assert_eq!(p.state_bv,BitVec::from_bytes(&[0xc0]));
-        assert_eq!(p.rem_len, 1.0);
+        assert_eq!(p.total_len, 3);
         
-        p = Plank::new(3,4.0,&BitVec::from_bytes(&[0x50]),&pieces);
+        p = Plank::new(3,&BitVec::from_bytes(&[0x50]),&pieces);
         assert_eq!(p.state_bv,BitVec::from_bytes(&[0xf8]));
-        assert_eq!(p.rem_len, -5.0);
+        assert_eq!(p.total_len, 9);
 
-        p.set_num_pieces(2,4.0);
+        p.set_num_pieces(2);
         assert_eq!(p.state_bv,BitVec::from_bytes(&[0xf0]));
-        assert_eq!(p.rem_len, 0.0);
+        assert_eq!(p.total_len, 4);
     }
 
     #[test]
     fn test_next_function() {
-        let pieces = vec![1.0, 3.0, 6.0, 7.0, 8.0, 10.0,15.0,17.0, 20.0];
-        //  [0x45,0x0] =   0    1    0    0  |  0    1    0    1  |  0
-        let mut p = Plank::new(3,43.0,&BitVec::from_bytes(&[0x45,0x0]),&pieces);
-        let mut old_rem_len = 43.0;
-        let mut curr_rem_len = p.rem_len;
+        let pieces = vec![ 1, 3, 6, 7,  8,10,15,17, 20];
+        //  [0x45,0x0] =   0  1  0  0 | 0  1  0  1 | 0
+        let mut p = Plank::new(3,&BitVec::from_bytes(&[0x45,0x0]),&pieces);
+        let mut old_len = 0;
+        let mut curr_len = p.total_len;
         let mut counter = 1;
         while p.has_next() {
-            assert!(curr_rem_len <= old_rem_len);
+            assert!(old_len <= curr_len);
             p.next();
-            old_rem_len = curr_rem_len;
-            curr_rem_len = p.rem_len;
+            old_len = curr_len;
+            curr_len = p.total_len;
             counter += 1;
         }
         // 6 choose 3 = 20
